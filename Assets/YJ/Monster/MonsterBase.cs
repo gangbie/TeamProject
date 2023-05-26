23abfcd147fd6bc5b7d5dc7770ec635e4abf3601
영준 배 <byj6614@naver.com>
2023-05-26 16:10:50 +0900

    feat : Frog몬스터 구현 및 Die구현

--- Assets/YJ/Monster/MonsterBase.cs
+++ Assets/YJ/Monster/MonsterBase.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MonsterBase : MonoBehaviour
+public abstract class MonsterBase
 {
-   
+    public abstract void Enter();
+    public abstract void Update();
+
+    public abstract void Exit();
+    
 }
