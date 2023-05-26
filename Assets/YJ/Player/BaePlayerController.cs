fe638b7e423cbd1b71137ba106ed0da3c34fde5e
영준 배 <byj6614@naver.com>
2023-05-26 18:54:19 +0900

    feat : 무적 구현 및 점프제한

--- Assets/YJ/Player/BaePlayerController.cs
+++ Assets/YJ/Player/BaePlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.XR;
 
@@ -9,6 +10,12 @@ public class BaePlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float maxSpeed;
     [SerializeField] private float jumpPower;
+    private bool isGround = true;
+
+    public UnityEvent OnJumped;
+    public UnityEvent OnDied;
+    public UnityEvent OnScored;
+    //public UnityEvent OnDamage;
 
     private Vector2 inputDir;
     private SpriteRenderer rend;
@@ -43,7 +50,12 @@ public class BaePlayerController : MonoBehaviour
 
     public void Jump()
     {
-        rb.AddForce(Vector2.up*jumpPower, ForceMode2D.Impulse);
+        if (isGround)
+        {
+            rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+
+            isGround = false;
+        }
     }
     private void OnMove(InputValue value)
     {
@@ -55,15 +67,47 @@ public class BaePlayerController : MonoBehaviour
         Jump();
     }
 
+    //피격시 무적
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Monster")
+        {
+            OnDamaged(collision.transform.position);
+        }
+    }
+
+    public void OnDamaged(Vector2 targetPos)
+    {
+        gameObject.layer = 10;
+
+        rend.color = new Color(1, 1, 1 ,0.4f);
+
+        int dirc = transform.position.x - targetPos.x > 0 ? 1 : -1;
+        rb.AddForce(new Vector2(dirc,1)*3, ForceMode2D.Impulse);
+
+        Invoke("OffDamaged", 2);
+    }
+    public void OffDamaged()
+    {
+        gameObject.layer = 8;
+        rend.color = new Color(1, 1, 1, 1);
+    }
+
+    //점프시 모션 변경및 몬스 터 밟기
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        isGround = true;
         ani.SetBool("JumpBool", false);
         if(collision.gameObject.tag=="Monster")
-            rb.AddForce(Vector2.up*10.0f,ForceMode2D.Impulse);
+        {
+            rb.AddForce(Vector2.up * 10.0f, ForceMode2D.Impulse);
+        }
+            
 
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
+        
         ani.SetBool("JumpBool", true); 
     }
 }
